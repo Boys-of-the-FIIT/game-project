365671f58f3396042c49ba030b773b2324e4b81f
Ivan Fedorov <fedivmi@gmail.com>
2023-05-26 17:19:41 +0500

    bullet trails

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -13,11 +13,37 @@ namespace Bullet
         [SerializeField] private float maxDistance;
         [SerializeField] public int damage;
         [SerializeField] private BulletType type;
+        
+        [SerializeField] private BulletTrailScriptableObject trailConfig;
+        private TrailRenderer trail;
+
+        [SerializeField] private Renderer renderer;
+        private bool isDisabling;
         private Vector3 _startPosition;
+
+        private const string DISABLE_METHOD_NAME = "Disable";
+        private const string DO_DISABLE_METHOD_NAME = "DoDisable";
         
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
+            trail = GetComponent<TrailRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            renderer.enabled = true;
+            CancelInvoke(DISABLE_METHOD_NAME);
+            ConfigureTrail();
+            Invoke(DISABLE_METHOD_NAME, 2);
+        }
+
+        private void ConfigureTrail()
+        {
+            if (trail is not null && trailConfig is not null)
+            {
+                trailConfig.SetUpTrail(trail);
+            }
         }
 
         private void Start()
@@ -35,7 +61,7 @@ namespace Bullet
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.gameObject.tag == Tags.Enemy && type == BulletType.PlayerBullet)
-            {   
+            {
                 var enemy = col.gameObject.GetComponent<Enemy>();
                 if (enemy.gameObject != col.gameObject)
                     enemy.TakeDamage(damage);
@@ -45,6 +71,34 @@ namespace Bullet
                 var player = col.gameObject.GetComponent<PlayerEntity>();
                 player.TakeDamage(damage);
             }
+        }
+
+        protected void Disable()
+        {
+            CancelInvoke(DISABLE_METHOD_NAME);
+            CancelInvoke(DO_DISABLE_METHOD_NAME);
+            rb.velocity = Vector3.zero;
+            renderer.enabled = false;
+
+            if (trail is not null && trailConfig is not null)
+            {
+                isDisabling = true;
+                Invoke(DO_DISABLE_METHOD_NAME, trailConfig.durationTime);
+            }
+            else
+            {
+                DoDisable();
+            }
+        }
+
+        private void DoDisable()
+        {
+            if (trail is not null && trailConfig is not null)
+            {
+                trail.Clear();
+            }
+            
+            gameObject.SetActive(false);
         }
     }
 }
