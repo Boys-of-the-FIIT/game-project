3c605190f5078ff961b78ccb3607aad338b3f9c7
Ivan Fedorov <fedivmi@gmail.com>
2023-05-31 22:32:59 +0500

    enemies dodging player's bullets

--- Assets/Scripts/Behaviours/KeepingDistanceFromPlayer.cs
+++ Assets/Scripts/Behaviours/KeepingDistanceFromPlayer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Player;
 using UnityEngine;
 using Zenject;
@@ -12,18 +14,34 @@ namespace Behaviours
         [SerializeField] private float minDistanceToPlayer = 5;
         [SerializeField] private float maxDistanceToPlayer = 10;
         [SerializeField] private float speed;
-        
+        [SerializeField] private float dodgingRadius = 8;
         private Transform player;
         private float angleToKeep;
         private float distanceToKeep;
         private float distanceToPlayer;
+
+        private float maxDodgingTime = 1.0f;
+        private float dodgingTime;
+        private bool isDodging;
+        private Rigidbody2D dodgedBullet;
+        private Vector3 bulletVelocity;
+        private Vector3 bulletVelocityOrthogonal;
+        private Vector3 playerToEnemy;
+
+        private Vector3 playerShootingPosition;
+
+        private HashSet<Collider2D> dodgedBullets = new();
+
+        private Vector3 prevPosition;
+        private Vector3 velocity;
+        private float direction;
 
         [Inject]
         private void Construct(PlayerEntity player)
         {
             this.player = player.transform;
         }
-        
+
         private void Start()
         {
             angleToKeep = Random.Range(-180f, 180f);
@@ -33,9 +51,28 @@ namespace Behaviours
 
         private void Update()
         {
-            if (!player) return;
-            distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            UpdateVelocity();
 
+            if (isDodging)
+            {
+                DodgeBullet();
+                return;
+            }
+
+            var colliders = Physics2D.OverlapCircleAll(transform.position, dodgingRadius);
+            var bullet = colliders.FirstOrDefault(coll => coll.CompareTag("Bullet"));
+
+            if (!isDodging && bullet is { name: "Player Bullet" or "Player Bullet(Clone)" }
+                           && dodgedBullets.Add(bullet))
+            {
+                UpdateDodgingData(bullet);
+                DodgeBullet();
+                return;
+            }
+
+            if (!player) return;
+
+            distanceToPlayer = Vector3.Distance(transform.position, player.position);
             var directionTowardsPlayer = player.position - transform.position;
 
             if (distanceToPlayer < minDistanceToPlayer)
@@ -53,6 +90,61 @@ namespace Behaviours
             if (distanceToPlayer <= maxDistanceToPlayer) return;
 
             ChasePlayer(directionTowardsPlayer);
+        }
+
+        private void UpdateDodgingData(Collider2D bullet)
+        {
+            isDodging = true;
+            dodgedBullet = bullet.GetComponent<Rigidbody2D>();
+            bulletVelocity = dodgedBullet.velocity;
+            bulletVelocityOrthogonal = Vector3.Cross(bulletVelocity, Vector3.forward);
+            playerShootingPosition = player.position;
+
+            playerToEnemy = transform.position - playerShootingPosition;
+
+            var playerToEnemyOrthogonal = Vector3.Cross(playerToEnemy, Vector3.forward);
+            var orthogonalDotBulletVelocity = Vector3.Dot(playerToEnemyOrthogonal, bulletVelocity);
+            var orthogonalDotEnemy = Vector3.Dot(playerToEnemyOrthogonal, velocity);
+
+            var sign = -orthogonalDotBulletVelocity * orthogonalDotEnemy;
+
+            direction = Mathf.Sign(sign);
+        }
+
+        private void UpdateVelocity()
+        {
+            velocity = (transform.position - prevPosition) / Time.deltaTime;
+            prevPosition = transform.position;
+        }
+
+        private void DodgeBullet()
+        {
+            dodgingTime += Time.deltaTime;
+
+            if (dodgingTime > maxDodgingTime)
+            {
+                StopDodging();
+                return;
+            }
+
+            var enemyPosition2d = new Vector2(transform.position.x, transform.position.y);
+            var enemyToBullet = dodgedBullet.position - enemyPosition2d;
+
+            var dot = Vector3.Dot(enemyToBullet, bulletVelocity);
+
+            if (dot > 0)
+            {
+                StopDodging();
+                return;
+            }
+
+            transform.position += direction * bulletVelocityOrthogonal.GetDirectionWithSpeed(speed);
+        }
+
+        private void StopDodging()
+        {
+            isDodging = false;
+            dodgingTime = 0;
         }
 
         private void ChasePlayer(Vector3 directionTowardsPlayer)
